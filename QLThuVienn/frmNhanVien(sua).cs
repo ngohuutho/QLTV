b894cee2e78143a01dc44f3a08ed4637b57c28d7
ngohuutho <32090598+ngohuutho@users.noreply.github.com>
2017-10-04 08:54:35 +0700

    thọ xửa file nhân viên
    hhhhh

--- QLThuVienn/frmNhanVien(sua).cs
+++ QLThuVienn/frmNhanVien(sua).cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-namespace QLThuVien
+namespace QLThuViennhom1
 {
     public partial class frmNhanVien : Form
     {
